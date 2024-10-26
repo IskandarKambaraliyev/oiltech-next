4234ce9496662a009c7e759afb1688b6c0771913
Iskandar Kambaraliyev <human.github@gmail.com>
2024-10-26 15:14:11 +0500

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,49 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Blue colors
+        "blue-750": "var(--color-blue-750)",
+        "blue-main": "var(--color-blue-main)",
+        "blue-600": "var(--color-blue-600)",
+        "blue-500": "var(--color-blue-500)",
+        "blue-400": "var(--color-blue-400)",
+        "blue-300": "var(--color-blue-300)",
+        "blue-200": "var(--color-blue-200)",
+        "blue-100": "var(--color-blue-100)",
+
+        // Green colors
+        "green-main": "var(--color-green-main)",
+        "green-600": "var(--color-green-600)",
+        "green-500": "var(--color-green-500)",
+        "green-400": "var(--color-green-400)",
+        "green-300": "var(--color-green-300)",
+        "green-200": "var(--color-green-200)",
+        "green-100": "var(--color-green-100)",
+
+        // White colors
+        "white-main": "var(--color-white-main)",
+        "white-600": "var(--color-white-600)",
+        "white-500": "var(--color-white-500)",
+        "white-400": "var(--color-white-400)",
+        "white-300": "var(--color-white-300)",
+        "white-200": "var(--color-white-200)",
+        "white-100": "var(--color-white-100)",
+      },
+      boxShadow: {
+        "shadow-btn": "var(--shadow-btn)",
+      },
+      screens: {
+        lg: "1280px",
+        md: "768px",
+        xl: "1600px",
+      },
+      zIndex: {
+        "header-1": "4",
+        header: "5",
+        "header+1": "6",
+        "modal-1": "9",
+        modal: "10",
+        "modal+1": "11",
       },
     },
   },
